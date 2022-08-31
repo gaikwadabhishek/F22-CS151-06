70088822777d4c9609d1fcb315efaac4fb29e38a
akshtshrma24 <akshitsharmaa65@gmail.com>
2022-08-30 20:18:09 -0700

    hw1 Q1

---
+++ src/hw1/Sharma.java
@@ -0,0 +1,8 @@
+package hw1;
+
+public class Sharma {
+    public static void main(String[] args){
+        Student stud = new Student("Akshit Sharma", "I like listening to peezy and I cut hair", 9734);
+        // Rio Da Yung OG
+    }
+}
