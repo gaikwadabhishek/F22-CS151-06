6c00fc8c726e992084130f4cbf6b081371a38838
Sarthak Dhomne <sarthakdhomne@gmail.com>
2022-09-03 15:08:24 -0700

    Added DhomneTest.java

---
+++ src/hw1/DhomneTest.java
@@ -0,0 +1,9 @@
+package hw1;
+
+public class DhomneTest {
+
+	public static void main(String[] args) {
+		Student Sarthak = new Student("Sarthak Dhomne", "Hi, I'm a 3rd-year SJSU student.", 9028);
+	}
+		
+}
