f3dd5e0725cbe3f7ecda030a2c6ad7d69f2d7c15
Ourasa <111930214+Ourasa@users.noreply.github.com>
2022-09-04 16:38:13 -0700

    Create PhamTest.java

---
+++ src/hw1/PhamTest.java
@@ -0,0 +1,9 @@
+package hw1; 
+
+public class PhamTest {
+ 
+  public static void main(String[] args) {
+    Student me = new Student("Steven", "My name is Steven, and I am a normal person, probably.", 6659);  
+  }
+  
+} 
