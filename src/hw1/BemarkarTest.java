d016de9e592501578f41027b5819282eb5dfbe2b
Arjun <ccldarjun@icloud.com>
2022-09-04 18:19:21 -0700

    Create BemarkarTest.java

---
+++ src/hw1/BemarkarTest.java
@@ -0,0 +1,9 @@
+package hw1;
+
+public class BemarkarTest {
+	public static void main(String[] args) {
+		Student arjun = new Student("Arjun",
+									"I like hiking and programming!",
+									9883);
+	}
+} 
