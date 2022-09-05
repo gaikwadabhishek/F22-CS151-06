e09ac36e4888c2fc6f9680653e8e283738c83b5b
Keerthanaa Ellur <57739452+Kellur0@users.noreply.github.com>
2022-09-05 16:24:00 -0700

    Add files via upload

---
+++ src/hw1/EllurTest.java
@@ -0,0 +1,13 @@
+package hw1;
+
+public class EllurTest {
+	public static void main(String[] args) 
+	{//Comment
+		//Student ID - 014643187
+	//Watched alot of GitHub tutorial videos and asked my friend for help in GitHub. I am still not comfortable in using GitHub.
+		
+		Student student = new Student ("Keerthanaa Ellur", "Hi everyone, I am a graduating senior.", 3187);
+	
+	}
+
+}
