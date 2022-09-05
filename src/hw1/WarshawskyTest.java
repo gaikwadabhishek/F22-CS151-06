1757d0299d1bc36c526a357e62a9cd57de7508c4
davidwarshawsky <davidawarshawsky@gmail.com>
2022-09-05 12:55:36 -0700

    Added WarshawskyTest.java

---
+++ src/hw1/WarshawskyTest.java
@@ -0,0 +1,13 @@
+package hw1;
+
+public class WarshawskyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// My full student ID doesn't work in this set up because my student ID starts
+		// with 0 and Java ints can't start with 0.
+		Student David = new Student("David Warshawsky","Hi I'm David",14082731);
+
+	}
+
+}
