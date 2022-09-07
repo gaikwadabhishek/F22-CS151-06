8cfb1f94abe7c98b34d1dd1c553577bcb321d76d
camiashley <54650324+camiashley@users.noreply.github.com>
2022-09-07 14:33:04 -0700

    Create HawkinsTest.java

---
+++ src/hw1/HawkinsTest.java
@@ -0,0 +1,12 @@
+package hw1;
+
+public class HawkinsTest {
+public static void main(String[] args) {
+	Student st1 = new Student ("Cami Hawkins","Allow me to reintroduce myself", 8573 );
+	System.out.println(st1.getName());
+	System.out.println(st1.getIntroduction());
+	System.out.println(st1.getStudentId());
+	
+		
+}
+}
