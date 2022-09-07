941d22f830daf69476bfd7db5c1849cd2de1b7d2
Brendan Requierme <48746100+brendanrequierme@users.noreply.github.com>
2022-09-07 16:21:10 -0700

    Create RequiermeTest.java

---
+++ src/hw1/RequiermeTest.java
@@ -0,0 +1,13 @@
+//Q1
+package hw1;
+
+public class RequiermeTest{
+    
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.setName("Brendan Requierme");
+        student1.setIntroduction("Hey! I'm a gamer.");
+        student1.setStudentId(2036);
+    }
+
+}
