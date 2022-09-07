441a785110671d8a6c97035d3f2fe2a7b0a1ac1c
RyanYavari <48726914+RyanYavari@users.noreply.github.com>
2022-09-07 14:06:00 -0700

    Add files via upload

---
+++ src/hw1/YavariTest.java
@@ -0,0 +1,8 @@
+package hw1;
+
+public class YavariTest {
+	public static void main(String[] args) {
+		Student ryan = new Student("Ryan Yavari", "Hello, I am a sophmore studying Computer Science at SJSU.", 5796);
+	}
+
+}
