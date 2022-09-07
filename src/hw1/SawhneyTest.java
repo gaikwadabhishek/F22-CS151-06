6ddb690fd80cd1e292c1908134dc38f257f15a69
asawhney962 <anubhav.sawhney1@gmail.com>
2022-09-06 19:10:16 -0700

    Create SawhneyTest.java

---
+++ src/hw1/SawhneyTest.java
@@ -0,0 +1,39 @@
+package hw1;
+
+public class {
+  private final String name;
+  private final String id;
+  private final String loginID;
+  private final int year 
+  
+public Student ( String name, String id, String loginID, int year){
+    this.name = name;
+    this.id = id;
+    this.loginID = loginID;
+    this.year = year;
+    
+    System.out.println("Student with name " + name);
+    
+    public Student( String name, String id, String loginID){
+      this(name, id,loginID,1);
+    }
+    public String getName(){
+      return name;
+    }
+    public String getid(){
+      return id;
+    }
+    public String getLoginID(){
+      return loginID;
+    }
+    public int getYear(){
+      return year;
+    }
+public static void main(String arg){
+  Student s1 = new Student("8755", "student@SJSU.edu");
+  Student s2 = new Student("6534", "student@SJSU.edu");                       
+}
+    
+  }
+  
+}
